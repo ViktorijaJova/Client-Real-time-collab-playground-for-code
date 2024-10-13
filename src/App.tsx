6b39bbf84175fffe9ec4fd6bcd81c186ead8f517
ViktorijaJova <viktorija@sols.mk>
2024-10-13 16:33:42 +0200

    added heroku backend

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from 'react';
 import { Editor } from '@monaco-editor/react'; // Correct import
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:4000'); // Change to your backend URL in production
+// Replace with your Heroku backend URL
+const socket = io('https://obscure-retreat-63973-92abc2c62e6e.herokuapp.com'); // Change to your backend URL
+//const socket = io('http://localhost:4000'); // Change to your backend URL in production
 
 const App: React.FC = () => {
     const [code, setCode] = useState<string>(''); // State to store the code
@@ -27,7 +29,6 @@ const App: React.FC = () => {
             socket.emit('codeChange', newValue); // Emit code change to the server
         }
     };
-    
 
     return (
         <div style={{ height: '100vh' }}>
